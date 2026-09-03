3d932d5db2d73571b0fb539a30b95670e95e6938
Suliman <sulimanyounis81@gmail.com>
2026-09-03 14:51:38 +0300

    update database

--- authproject/settings.py
+++ authproject/settings.py
@@ -60,8 +60,12 @@ WSGI_APPLICATION = "authproject.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "BGroth",      
+        "USER": "postgres",          
+        "PASSWORD": "3650681",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
